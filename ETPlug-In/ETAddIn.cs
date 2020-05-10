71e0e0bbeb43d3ff339e82d1386de30224b5a31f
yanyixin <15201231344@163.com>
2020-05-10 22:44:54 +0800

    添加ETPlug-In项目引用

--- ETPlug-In/ETAddIn.cs
+++ ETPlug-In/ETAddIn.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AddInDesignerObjects;
+using Office;
+using Excel;
 
 namespace ETPlug_In
 {
-    public class ETAddIn
+    public class ETAddIn: IDTExtensibility2, IRibbonExtensibility
     {
+        public void OnConnection(object Application, ext_ConnectMode ConnectMode, object AddInInst, ref Array custom)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnDisconnection(ext_DisconnectMode RemoveMode, ref Array custom)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnAddInsUpdate(ref Array custom)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnStartupComplete(ref Array custom)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnBeginShutdown(ref Array custom)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public string GetCustomUI(string RibbonID)
+        {
+            //throw new NotImplementedException();
+            return "";
+        }
     }
 }
